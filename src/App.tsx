63425b136d1ae3f64636340c00929a4fe347be74
aagalich <aagalich@avito.ru>
2024-10-07 15:54:39 +0300

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -6,11 +6,11 @@ amplitude.init('2082ab2a4b7ab2b8cdd162f713b2798', {"autocapture":true});
 function App() {
    const [count, setCount] = useState(0);
    amplitude.setUserId('test@user')
-
+    console.log(count)
     return (
         <div>
            TEST
-            <button onClick={() =>setCount(prevState => prevState++)}>Click me</button>
+            <button onClick={() =>setCount(prevState => prevState + 1)}>Click me</button>
             <span>{count}</span>
         </div>
     );
